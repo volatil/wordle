792a69c4a2b73c2c43a91e402986e91422ea4a96
Paulo Torrealba <contacto@volatil.cl>
2024-09-08 11:49:52 -0300

    ordenando ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,15 @@
-$("nav.menu[ data-menu='fondo' ], nav[ data-menu='contenido' ] > .cerrar").on("click", function(){
-    $( "nav.menu[ data-menu='fondo' ]" ).toggle();
-    $( "nav.menu[ data-menu='contenido' ]" ).toggle();
-})
-$("header .hamburguesa").on("click", function(){
-    $( "nav.menu[ data-menu='fondo' ]" ).toggle();
-    $( "nav.menu[ data-menu='contenido' ]" ).toggle();
-})
+// TOGGLE MENU
+{
+    $("nav.menu[ data-menu='fondo' ], nav[ data-menu='contenido' ] > .cerrar").on("click", function(){
+        $( "nav.menu[ data-menu='fondo' ]" ).toggle();
+        $( "nav.menu[ data-menu='contenido' ]" ).toggle();
+    })
+    $("header .hamburguesa").on("click", function(){
+        $( "nav.menu[ data-menu='fondo' ]" ).toggle();
+        $( "nav.menu[ data-menu='contenido' ]" ).toggle();
+    })
+}
+
+// CLICK MENU
+{
+}
