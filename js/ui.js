10e01c53e66ab1d0e1084668b4aca87d3e28ad96
Paulo Torrealba <contacto@volatil.cl>
2024-09-09 03:08:55 -0300

    preparando instrucciones

--- js/ui.js
+++ js/ui.js
@@ -1,22 +1,22 @@
 import { reiniciarJuego } from "./main.js";
 
 
-const cerrarMenu = function() {
+const menuToggle = function() {
     $( "nav.menu[ data-menu='fondo' ]" ).toggle();
     $( "nav.menu[ data-menu='contenido' ]" ).toggle();
 }
-const abrirMenu = function() {
-    $( "nav.menu[ data-menu='fondo' ]" ).toggle();
-    $( "nav.menu[ data-menu='contenido' ]" ).toggle();
+const modalToggle = function() {
+    $( ".elmodal[ data-tipo='fondo' ]" ).toggle();
+    $( ".elmodal[ data-tipo='contenido' ]" ).toggle();
 }
 
 // MENU => TOGGLE
 {
     $("nav.menu[ data-menu='fondo' ], nav[ data-menu='contenido' ] > .cerrar").on("click", function(){
-        cerrarMenu();
+        menuToggle();
     })
     $("header .hamburguesa").on("click", function(){
-        abrirMenu();
+        menuToggle();
     })
 }
 
@@ -28,11 +28,26 @@ const abrirMenu = function() {
         switch ( trigger ) {
             case "reiniciar":
                 reiniciarJuego();
-                cerrarMenu();
+                menuToggle();
                 break;
             case "merindo":
                 const palabra = $("span.palabra").html();
                 alert(`La palabra era: ${ palabra }`)
+                break;
+            case "instrucciones":
+                $(".elmodal[ data-tipo='contenido' ] > .contenido").html(`
+                    <h5>Como jugar</h5>
+                    <p>Adivina la palabra en 6 instentos</p>
+                    <ul>
+                        <li>Cada suposición debe ser una palabra válida de 5 letras.</li>
+                        <li>El color de las fichas cambiará para mostrar qué tan cerca estuvo tu suposición de la palabra.</li>
+                    </ul>
+                    <div class="ejemplos">
+                        
+                    </div>
+                `);
+                menuToggle();
+                modalToggle();
                 break;
         
             default:
@@ -45,7 +60,6 @@ const abrirMenu = function() {
 // MODAL => TOGGLE
 {
     $(".elmodal[ data-tipo='fondo' ], .elmodal[ data-tipo='contenido' ] > .cerrar").on("click", function(){
-        $( ".elmodal[ data-tipo='fondo' ]" ).toggle();
-        $( ".elmodal[ data-tipo='contenido' ]" ).toggle();
+        modalToggle();
     });
 }
